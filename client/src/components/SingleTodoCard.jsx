2d8638522301e4f277aed53aa610a35145303bb6
i am Sunil <bistasunil360@gmail.com>
2024-04-05 15:55:56 +0545

    add complet or not feature

--- client/src/components/SingleTodoCard.jsx
+++ client/src/components/SingleTodoCard.jsx
@@ -1,15 +1,22 @@
-import React from "react";
+import { useState } from "react";
 import { BsTrashFill, BsCheckSquare } from "react-icons/bs";
 import { FaEdit } from "react-icons/fa";
 
 const SingleTodoCard = (props) => {
+  const [done, setDone] = useState(false);
   return (
     <div className="flex justify-between bg-red-100 py-2 rounded shadow">
       <div className="px-4">
-        <h1>{props.name}</h1>
+        <h1 className={done ? " font-semibold line-through" : " font-semibold"}>
+          {props.name}
+        </h1>
       </div>
       <div className="px-4 flex space-x-4">
-        <BsCheckSquare className="cursor-pointer text-green-700" size={20} />
+        <BsCheckSquare
+          onClick={() => setDone(!done)}
+          className="cursor-pointer text-green-700"
+          size={20}
+        />
         <FaEdit className="cursor-pointer text-yellow-700" size={20} />
         <BsTrashFill className="cursor-pointer text-red-700" size={20} />
       </div>
